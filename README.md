c5bc300751c81c742018a33b984e1c77c9ab1340
Dimitar Dzhongov <djongov@gamerz-bg.com>
2022-08-16 14:46:10 +0300

    Major Update
    The library has been re-worked to not require a .css file or an icon file. It is now just a single js file
    Also, added a new icon and now has customization options

--- README.md
+++ README.md
@@ -1,38 +1,37 @@
 # cClipboard
-cClipboard is a small Javascript library that creates a copy to clipboard button to all elements with the class="c0py".
+cClipboard is a very small and incredibly simple to use Javascript library that creates and appends a copy to clipboard button to all elements with the class="c0py".
+
+Original Size:	2.3KB gzipped (6.99KB uncompressed) Compiled Size:	1.33KB gzipped (4.04KB uncompressed)
+
+And that's with comments on every line!
 
 ## Installation through CDN
 
-All you have to do is insert the script from a CDN and start adding class="c0py" to the html elements you want to have a copy to clipboard on
+All you have to do is insert the script from the CDN and the bottom (or at least after the copy elements would have rendered) of your ```html <body> ``` and start adding ```html class="c0py"``` to the html elements you want to have a copy to clipboard on
 
 ```html
-<script src="https://cdn.jsdelivr.net/gh/Djongov/cClipboard@v1.0.1/cClipboard.min.js" integrity="sha384-N75xYewFG0GqtAc5o5bbXvYjs3pxwDndG9KhOWEltGXfqlx7MKOdIB6FLifDxF+a" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/gh/Djongov/cClipboard@v2.0/cClipboard.min.js" integrity="sha384-N75xYewFG0GqtAc5o5bbXvYjs3pxwDndG9KhOWEltGXfqlx7MKOdIB6FLifDxF+a" crossorigin="anonymous"></script>
 ```
+## Usage
+Just add a class "c0py" to the element you want to copy text from and that's it!
 
-The JS file will load automatically the following:
+## Customizations
+You are able to customize the color of the svg icon (stroke) and the stroke-width. All you have to is to pass a data attribute to the c0py elementm along with the c0py class.
 
-CSS stylesheet https://cdn.jsdelivr.net/gh/Djongov/cClipboard/cClipboard.css"
+Example:
 
-The image will be delivered from https://djo.blob.core.windows.net/cclipboard/clipboard.svg
+```html <p class="c0py" data-clipboard-icon-stroke="#ff4500" data-clipboard-icon-stroke-width="1.5">Copy me!</p> ```
 
-## Manual installation
-If you want to have more control over the script, download the js, css and svg file from the repo and insert them into your html
+Best is to use strok-with between 1 and 3.
 
-cClipboard.js - this is the main file
-cClipboard.css - styling the tooltip
-cClipboard.svg - the image that is used as a button
-So insert the cClipboard.js
+## How it works
+Script looks for DOM elements with class "c0py". Then it creates a couple of new elements like a div, a svg icon and a tooltip div and appends them as the next element after the element with the class "c0py".
 
-```html
-<script src="./cClipboard.js"></script>
-```
-Package is comprised of 3 parts
+## References
+The SVG icon comes from https://tablericons.com/
 
-It will automatically load the cClipboard.css only if there are elements with class="c0py" on the document. Important think to note is where you place the cClipboard.css file. It assumes that it is in the same directory but if not, edit the code on line 18 in cClipboard.js with the proper path. Another think to consider is where you place your cClipboard.svg image. Line 35 in cClipboard.js controls the path, by default it's in the same folder as the .js script as well.
-
-That's it.
-
-Then you just add "c0py" class to any element that you want to have a button to copy to clipboard
-
-## To Do
-Tooltip overflows if the image element is on the left: 0 position
+## Improvements
+Some areas where we can improve the library:
+- Tooltip positioning - Tooltip overflows if the copy icon is on the left: 0 position
+- Icon positioning
+- Mabye a better way to render the icons
--- cClipboard.js
+++ cClipboard.js
@@ -1,48 +1,108 @@
-// Load the buildCopy function on window load
-window.onload = buildCopy;
-
 // function to go through all the items with .c0py class and build tooltips and clipboard copy image + event
 function buildCopy() {
-    // Query all the elements on the DOM with class c0py
+  // Query all the elements on the DOM with class c0py
     const copyDivs = document.querySelectorAll('.c0py');
-    // Log them to the console
-    console.log("Found " + copyDivs.length + " C0py elements")
     // Start the whole deal only if there are elements with class "c0py" in the DOM
     if (copyDivs.length > 0) {
-        // Add the css stylesheet
-        let headID = document.getElementsByTagName('head')[0];
-        let link = document.createElement('link');
-        link.type = 'text/css';
-        link.rel = 'stylesheet';
-        link.integrity = 'sha384-hVlBRriMBCEJtd/pB9l9Q2W732YOI67xBihNoe4760AzTiAB9ECIiVplgM5OH8ht';
-        link.crossOrigin = 'anonymous';
-        // Change this to the location where you will place the cClipboard.css
-        link.href = 'https://cdn.jsdelivr.net/gh/Djongov/cClipboard/cClipboard.css';
-        headID.appendChild(link);
+        // Log them to the console
+        console.log("Found " + copyDivs.length + " C0py elements");
+        // Tooltip styles - will be appended to the end of <head>
+        var css = `
+/* Tooltips Settings */
+.Ctooltip {
+    position: relative;
+    display: inline-block;
+}
+.Ctooltip .Ctooltiptext {
+    visibility: hidden;
+    width: 140px;
+    background-color: black;
+    color: whitesmoke;
+    text-align: center;
+    border-radius: 6px;
+    padding: 5px;
+    position: absolute;
+    z-index: 1;
+    bottom: 150%;
+    left: 50%;
+    margin-left: -75px;
+    word-wrap: break-word;
+    display: inline-block;
+}
+.Ctooltip .Ctooltiptext::after {
+    content: "";
+    position: absolute;
+    top: 100%;
+    left: 50%;
+    margin-left: -5px;
+    border-width: 5px;
+    border-style: solid;
+    border-color: white transparent transparent transparent;
+}
+.Ctooltip:hover .Ctooltiptext {
+    visibility: visible;
+    opacity: 1;
+}
+        `;
+        // Create the style tag
+        var style = document.createElement('style');
 
+        if (style.styleSheet) {
+            style.styleSheet.cssText = css;
+        } else {
+            style.appendChild(document.createTextNode(css));
+        }
+        // Append to <head>
+        document.getElementsByTagName('head')[0].appendChild(style);
         // Loop through all of the found c0py elements
         for (i = 0; i < copyDivs.length; ++i) {
             // create the div that will encompass the entire clipboard image + tooltip
             const createTooltipDiv = document.createElement('div');
             // add class tooltip
-            createTooltipDiv.classList.add('cCtooltip');
+            createTooltipDiv.classList.add('Ctooltip');
             // insert it right after the c0py element
             copyDivs[i].parentNode.insertBefore(createTooltipDiv, copyDivs[i].nextSibling);
             // save the tooltip div to a variable for later use
-            const tooltipDiv = document.getElementsByClassName('cCtooltip')[i];
-            // Let's create the clipboard image
-            const clipboardImg = new Image(35, 35);
-            // Add the c0py-icon class which serves purpose for the onclick event later
+            const tooltipDiv = document.getElementsByClassName('Ctooltip')[i];
+            // We need a little margin on the right because the icon will be too close to the previous element
+            tooltipDiv.style.margin = "0 0.4rem";
+            // Let's create the SVG icon
+            // Default is black
+            let iconStroke = '#000';
+            // Default 1.5 seems clean
+            let strokeWidth = 1.5;
+            // Check if data attributes have been passed, like the stroke color (data-clipboard-icon-stroke) and stroke width (data-clipboard-icon-stroke-width)
+            if (copyDivs[i].dataset.clipboardIconStroke) {
+                //console.log('Prefered Icon Stroke is ' + copyDivs[i].dataset.clipboardIconStroke);
+                iconStroke = copyDivs[i].dataset.clipboardIconStroke;
+            }
+            if (copyDivs[i].dataset.clipboardIconStrokeWidth) {
+                //console.log('Prefered Icon Stroke width is ' + copyDivs[i].dataset.clipboardIconStrokeWidth);
+                strokeWidth = copyDivs[i].dataset.clipboardIconStrokeWidth;
+            }
+            // The icon itself - taken from https://tablericons.com/
+            let clipboardImgSource = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><rect x="8" y="8" width="12" height="12" rx="2" /><path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2" /></svg>';
+            // Because we will be creating the SVG from a string, we need a parent element
+            let div = document.createElement('div');
+            // Append it to the new div
+            div.innerHTML = clipboardImgSource;
+            // Add it as a child of the tooltip main div
+            tooltipDiv.appendChild(div);
+            // Now actually get the new SVG element as an HTML Node element
+            let clipboardImg = div.firstElementChild;
+            // Add it the c0py-icon class (needed later)
             clipboardImg.classList.add('c0py-icon');
-            clipboardImg.src = 'https://djo.blob.core.windows.net/cclipboard/clipboard.svg';
-            clipboardImg.alt = 'Copy this reference to Clipboard';
-            clipboardImg.style.marginTop = "10px";
-            // Add it as a child of the tooltip div
-            tooltipDiv.appendChild(clipboardImg);
+            // Do a widht and height (24 seems most approprite across the sizes)
+            clipboardImg.style.width = 24;
+            clipboardImg.style.height = 24;
+            // Apply the stroke
+            clipboardImg.style.stroke = iconStroke;
+            // Apply the stroke-width
+            clipboardImg.style.strokeWidth = strokeWidth;
             // create a <span> that will hold the tooltip text "Copy to clipboard"
             const createTooltipText = document.createElement('span');
             // Add tooltiptext class to it for styling
-            createTooltipText.classList.add('cCtooltiptext');
+            createTooltipText.classList.add('Ctooltiptext');
             // Set the text to Copy to Clipboard
             createTooltipText.innerHTML = "Copy to clipboard";
             // Add it as a child of the tooltip div
@@ -50,39 +110,38 @@ function buildCopy() {
             // Save the text value of the c0py element that will need saving to clipboard, for usre in the copyToClipboard function
             const text = copyDivs[i].innerHTML;
             //  Save the tooltip span element so it can be used in the copyToClipboard function (to change the text to Copied! after clicking)
-            const element = document.getElementsByClassName('cCtooltiptext')[i];
+            const element = document.getElementsByClassName('Ctooltiptext')[i];
             // Set the onclick event listener on the clipboard icon
-            document.getElementsByClassName('c0py-icon')[i].addEventListener("click", function() {
-                copyToClipboard(text, element)
-            }, false);
-
+            document.getElementsByClassName('c0py-icon')[i].addEventListener("click", function() {copyToClipboard(text, element)}, false);
+    
             // Now the actual copy to clipboard function
-            function copyToClipboard(text, element) {
-                // use the new ClipboardEvent API
-                if (window.clipboardData && window.clipboardData.setData) {
-                    // IE specific code path to prevent textarea being shown while dialog is visible.
-                    return clipboardData.setData("Text", text);
-                    // If the new one is not supported, try the old one with execCommand("copy")
-                } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
-                    var textarea = document.createElement("textarea");
-                    textarea.textContent = text;
-                    textarea.style.position = "fixed"; // Prevent scrolling to bottom of page in MS Edge.
-                    document.body.appendChild(textarea);
-                    textarea.select();
-
-                    try {
-                        return document.execCommand("copy");
-                    } catch (ex) {
-                        console.warn("Copy to clipboard failed.", ex);
-                        element.innerHTML = "Failed to Copy";
-                        return false;
-                    } finally {
-                        document.body.removeChild(textarea);
-                        element.innerHTML = "Copied";
-                        console.log("Copied: " + text);
-                    }
+            function copyToClipboard (text, element) {
+            // use the new ClipboardEvent API
+            if (window.clipboardData && window.clipboardData.setData) {
+                // IE specific code path to prevent textarea being shown while dialog is visible.
+                return clipboardData.setData("Text", text); 
+                // If the new one is not supported, try the old one with execCommand("copy")
+            } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+                var textarea = document.createElement("textarea");
+                textarea.textContent = text;
+                textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in MS Edge.
+                document.body.appendChild(textarea);
+                textarea.select();
+                try {
+                    return document.execCommand("copy");
+                } catch (ex) {
+                    console.warn("Copy to clipboard failed.", ex);
+                    element.innerHTML = "Failed to Copy";
+                    return false;
+                } finally {
+                    document.body.removeChild(textarea);
+                    element.innerHTML = "Copied";
+                    console.log("Copied: " + text);
                 }
+            }
             }
         }
     }
 }
+// Run the function
+buildCopy();
