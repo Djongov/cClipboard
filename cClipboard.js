3cdaf46872da6bbd8046afe38dbf7f9723e6ff18
Dimitar Dzhongov <djongov@gamerz-bg.com>
2022-08-21 00:50:25 +0300

    Update cClipboard.js
    Attempt to fix some sizing issues on the icon

--- cClipboard.js
+++ cClipboard.js
@@ -81,7 +81,7 @@ function buildCopy() {
                 strokeWidth = copyDivs[i].dataset.clipboardIconStrokeWidth;
             }
             // The icon itself - taken from https://tablericons.com/
-            let clipboardImgSource = '<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" viewBox="0 0 24 24" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><rect x="8" y="8" rx="2" /><path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2" /></svg>';
+            let clipboardImgSource = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><rect x="8" y="8" width="12" height="12" rx="2" /><path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2" /></svg>';
             // Because we will be creating the SVG from a string, we need a parent element
             let div = document.createElement('div');
             // Append it to the new div
